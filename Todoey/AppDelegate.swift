3515fb1a25c947cc8116f0ec8801bebaf7e5bca0
Rafael Santos <rafaelcsan705@gmail.com>
2023-01-16 21:20:25 +0000

    Lesson/268

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -18,13 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
 
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error trying to initialize Realm")
         }
-        
-//        print(Realm.Configuration.defaultConfiguration.fileURL)
-        
         return true
     }
 
